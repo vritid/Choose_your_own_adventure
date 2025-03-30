3d505b9194646d5a4dc4781ff8b480ad62875d68
Vriti Dahiya <141448040+vritid@users.noreply.github.com>
2025-03-29 23:59:47 -0400

    added licensing

--- Data/game_entities.py
+++ Data/game_entities.py
@@ -2,25 +2,16 @@
 
 Instructions (READ THIS FIRST!)
 ===============================
-
-This Python module contains the entity classes for Project 1, to be imported and used by
- the `adventure` module.
- Please consult the project handout for instructions and details.
-
 Copyright and Usage Information
 ===============================
 
-This file is provided solely for the personal and private use of students
-taking CSC111 at the University of Toronto St. George campus. All forms of
-distribution of this code, whether as given or with any changes, are
-expressly prohibited. For more information on copyright for CSC111 materials,
-please consult our Course Syllabus.
+All rights reserved. You may not use, modify, or distribute this code without written permission from the author.
 
-This file is Copyright (c) 2025 CSC111 Teaching Team
+This file is Copyright (c) 2025 Vriti Dahiya
+"""
 """
 from dataclasses import dataclass
 import random
-
 
 @dataclass
 class Location:
